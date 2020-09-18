59d8eeb0e1ef4df23c5ff68a113b44be027cc7b1
Andreia Trigueiro <trigueiro.andreia@gmail.com>
2020-09-18 13:58:40 +0100

    Adding video on background

--- curiousaddict-website/src/components/Navbar.js
+++ curiousaddict-website/src/components/Navbar.js
@@ -1,59 +1,63 @@
-import React, { useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from './Button';
-import './Navbar.css';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./Button";
+import "./Navbar.css";
 
 function Navbar() {
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
-    const handleCLick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
-    const showButton = () => {
-        if (window.innerWidth <= 960) {
-            setButton(false);
-        } else {
-            setButton(true);
-        }
-    };
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+  const handleCLick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
 
-    useEffect(() => {
-        showButton()
-    }, []);
+  useEffect(() => {
+    showButton();
+  }, []);
 
-    window.addEventListener('resize', showButton);
+  window.addEventListener("resize", showButton);
 
-    return (
-        <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                        <i className="fas fa-dice-d20"/> CURIOUS ADDICT
-                    </Link>
-                    <div className="menu-icon" onClick={handleCLick}>
-                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
-                    </div>
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className='nav-item'>
-                            <Link to="/" className='nav-links' onClick={closeMobileMenu}>
-                                Home
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to="/posts" className='nav-links' onClick={closeMobileMenu}>
-                                Posts
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to="/contact" className='nav-links' onClick={closeMobileMenu}>
-                                Contact
-                            </Link>
-                        </li>
-                    </ul>
-                    {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
-                </div>
-            </nav>
-        </>
-    )
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            <i className="fas fa-dice-d20" /> CURIOUS ADDICT
+          </Link>
+          <div className="menu-icon" onClick={handleCLick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/posts" className="nav-links" onClick={closeMobileMenu}>
+                Posts
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contact"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
