5e9c780b1460299ad7f002056f61aca4e0c8a6d5
Andreia Trigueiro <trigueiro.andreia@gmail.com>
2020-11-22 00:40:54 +0000

    corrections on personalinfo

--- curiousaddict-website/src/components/PersonalInfo.js
+++ curiousaddict-website/src/components/PersonalInfo.js
@@ -5,12 +5,13 @@ import "./PersonalInfo.css";
 function PersonalInfo() {
   return (
     <div className="about-container">
-      <h1>Hey there =)</h1>
+      <h1>Hey there</h1>
       <p>
       Besides being a Curious Addicted I also love to learn by trying and that is 
-      the reason why this page exist &#128516; .This is a very simple website
-      that I created to play with React :) <br></br> Any new creation I will 
-      add here, keep an eye on it &#129322; &#128540;
+      the reason why this page exist - only to play with React. <span role="img" 
+      aria-label="face with out tongue and winking eye">&#128540;</span>  
+      <br></br> Any new learning/creation I will add here, so keep an eye on it! <span role="img" 
+      aria-label="smiling face with open mouth and smiling eyes">&#128516;</span>
       </p>
       </div>
     );
