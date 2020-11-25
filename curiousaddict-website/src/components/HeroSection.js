474acccaaec8e2a59e6be2984eb864a3c5ce09e4
Andreia Trigueiro <trigueiro.andreia@gmail.com>
2020-11-25 11:13:19 +0000

    remove videos

--- curiousaddict-website/src/components/HeroSection.js
+++ curiousaddict-website/src/components/HeroSection.js
@@ -5,7 +5,6 @@ import "./HeroSection.css";
 function HeroSection() {
   return (
     <div className="hero-container">
-      {/* <video src="/videos/video-1.mp4" autoPlay loop muted /> */}
       <h1>KEEP CURIOUS</h1>
       <p>and you will always have lots of interesting things to do</p>
     </div>
